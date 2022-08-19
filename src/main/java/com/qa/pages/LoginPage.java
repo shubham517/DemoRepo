b6b37c997911bc57dfd120bb4d121c4d22fe246f
Shubham <shubham517@gmail.com>
2022-08-19 23:21:44 +0530

    added search page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,15 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void loginPage()
+	{
+		System.out.println("login page");
+	}
+	
+	public void loginSec()
+	{
+		System.out.println("login sec");
+	}
 
 }
